28d5bc0cdad799daa8b5b48a3fff54c8baa1a4ac
Dani <d.kools1@outlook.com>
2020-09-22 11:14:17 +0200

    Header and Main content sections are done.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dani Kools</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+</head>
+<body>
+    <div class="background">
+        <div class="header">
+            <div class="container">
+                <a href="#" class="logo">
+                    Dani Kools
+                </a>
+                <div class="header-right">
+                    <a href="#">Home</a>
+                    <a href="#">Projects</a>
+                    <a href="#">About me</a>
+                </div>
+            </div>
+        </div>
+        <div class="main-content">
+            <h2>I'm a Creative Front and Back end Developer.</h2>
+            <p>I am a curious developer pasionated about new and exciting technologies. I started my career in ICT managment and gradually moved on to development.</p>
+            <div class="icons">
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-facebook"></i>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
